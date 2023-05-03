b7b7bc5a63c41c5e48b4ccddfa931c7bdae2deaa
Sebastian009w <ia3266595@gmail.com>
2023-05-02 22:05:05 -0500

    views

--- react/views.py
+++ react/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import TaskSerializers
+from .models import Task
 
 # Create your views here.
+
+
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializers
+    queryset = Task.objects.all()
